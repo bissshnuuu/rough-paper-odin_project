c442b109f2e5e8072a67cef9d06d0b3a16136af6
bissshnuuu <bishnupandey218@gmail.com>
2026-06-20 05:56:32 +0000

    Creaated the main playGame function and inside it each round function with their own functionality and displayed the winner or loser message

--- script.js
+++ script.js
@@ -22,3 +22,38 @@ do{
     }
 } while(userinp === "yes" || userinp === "y");
 
+function playGame(){
+    for(let i=0; i<5; i++){
+        const playerSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection,computerSelection);
+    }
+    function playRound(playerSelection,computerSelection){
+        if(playerSelection === computerSelection){
+            console.log(`It's a tie! Both chose ${playerSelection}`);
+        }
+        else if(playerSelection === "rock" && computerSelection === "scissors" ||
+                playerSelection === "paper" && computerSelection === "rock" ||
+                playerSelection === "scissors" && computerSelection === "paper"){
+
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+            humanScore++;
+        }
+        else{
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+            computerScore++;
+        }
+
+        console.log(`Current Score - You: ${humanScore}, Computer: ${computerScore}`);
+    }
+}
+
+if(humanScore > computerScore){
+    console.log("Congratulations! You won the game!");
+}
+else if(computerScore > humanScore){
+    console.log("Sorry! The computer won the game!");
+}
+else{
+    console.log("It's a tie game!");
+}
