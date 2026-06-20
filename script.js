049403673cd75c426132a856bb511a95be0134b3
bissshnuuu <bishnupandey218@gmail.com>
2026-06-20 05:53:45 +0000

    Function Created for the Computer Choice

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ let computerScore = 0;
 console.log("Welcome to Rock, Paper, Scissors!");
 console.log("There will be 5 rounds. May the best player win!");
 
+function getComputerChoice(){
+    const choices = ["rock","paper","scissors"];
+    const randomIndex = Math.floor(Math.random()*choices.length);
+    return choices[randomIndex];
+}
+
